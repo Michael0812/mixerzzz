00ba44f0e69d83d3148d50e7e5b2010a25ff3c31
Michael0812 <michalwesolowski11@gmail.com>
2020-02-17 22:44:27 +0000

    Add app.py file

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-from jinja2 import Template
+import os
+from flask import Flask, render_template, redirect, request, url_for
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def base():
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT', '8080')),
+            debug=True)
