876e9b7677aae5df1d47a4d9f8c51c5d6ece91dd
Michael0812 <michalwesolowski11@gmail.com>
2020-02-18 21:04:54 +0000

    Added index.html

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ from flask import Flask, render_template, redirect, request, url_for
 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'whiskyDB'
+app.config["MONGO_URI"] = "mongodb+srv://Michael0812:a7d2hp@myfirstcluster-2ro3f.mongodb.net/whiskyDB?retryWrites=true&w=majority"
 
 
 
 @app.route('/')
-def base():
-    return render_template('base.html')
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT', '8080')),
-            debug=False)
+            debug=True)
