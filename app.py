7f4ff9f4926389b4276ed88317591275ac3feeb8
Michael0812 <michalwesolowski11@gmail.com>
2020-02-22 22:37:17 +0000

    added routing

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ app.config["MONGO_DBNAME"] = 'whiskyDB'
 app.config["MONGO_URI"] = "mongodb+srv://Michael0812:a7d2hp@myfirstcluster-2ro3f.mongodb.net/whiskyDB?retryWrites=true&w=majority"
 
 
-
+# Home page
 @app.route('/')
 def index():
     return render_template('pages/index.html')
+
+# Drinks page
+@app.route('/drinks.html')
+def drinks():
+    return render_template('pages/drinks.html')
 
 
 if __name__ == '__main__':
