0f60c4daea96b70cb4c7744412990cdcb3e4c3af
Michael0812 <michalwesolowski11@gmail.com>
2020-02-29 16:41:23 +0000

    deleted unnecessary file and code

--- app.py
+++ app.py
@@ -1,28 +1,35 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
+from bson.objectid import ObjectId
 from flask_pymongo import PyMongo, pymongo
-import env as config
+
 
 app = Flask(__name__)
 
 # Configuration of database
-app.config["MONGO_DBNAME"] = 'whiskyDB'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = "mongodb+srv://Michael0812:a7d2hp@myfirstcluster-2ro3f.mongodb.net/task_manager?retryWrites=true&w=majority";
 
 
 mongo = PyMongo(app)
 
+@app.route('/')
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("pages/tasks.html", tasks=mongo.db.tasks.find())
+
 
 # Home page
-@app.route('/')
+
 @app.route('/index.html')
 def index():
     return render_template('pages/index.html')
 
+
 # Drinks page
 @app.route('/drinks.html')
 def drinks():
-    return render_template('pages/drinks.html')
+    return render_template('pages/drinks.html', tasks=mongo.db.tasks.find())
 
 # Register page
 @app.route('/register.html')
