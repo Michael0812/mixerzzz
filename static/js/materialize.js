876af2149e5ce303e154c335530b8b3bc6b4066b
Michael0812 <michalwesolowski11@gmail.com>
2020-03-03 11:46:30 +0000

    moved JS from base.html to materialize.js

--- static/js/materialize.js
+++ static/js/materialize.js
@@ -3,9 +3,10 @@
 // Initialization plugin
 
 // Navbar
-$(document).ready(function() {
-            $('.collapsible').collapsible();
-            $('select').material_select();
-            $(".button-collapse").sideNav();
-        });
+    $(document).ready(function() {
+        $('.collapsible').collapsible();
+        $('select').material_select();
+        $(".button-collapse").sideNav();
+        $('select').formSelect();
+    });
 
